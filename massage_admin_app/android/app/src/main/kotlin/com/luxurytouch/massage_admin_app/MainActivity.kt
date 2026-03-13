3e38e15d95d6a7235c3fa0d80e7fc60e22727a8f
Kung-Kelvin <kung-gomez-kelvin2604@mail.kyutech.jp>
2026-03-13 22:01:40 +0900

    fully working mobile app and chrome only. Cache and onlie connection to firebase services

---
+++ massage_admin_app/android/app/src/main/kotlin/com/luxurytouch/massage_admin_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.luxurytouch.massage_admin_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ massage_admin_app/lib/web_download_stub.dart
@@ -0,0 +1,5 @@
+// lib/web_download_stub.dart
+void downloadWebFile(String csvData, String fileName) {
+  // This remains empty. It's only here so the mobile compiler 
+  // has a function signature to look at without crashing.
+}
