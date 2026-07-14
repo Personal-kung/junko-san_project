331420d5b5c01faacedd1e2157719100a0ce134b
Personal-kung <kelvinmext1@gmail.com>
2026-07-14 00:44:07 +0000

    deleting previous project to start from zero

--- massage_admin_app/android/app/src/main/kotlin/com/luxurytouch/massage_admin_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.luxurytouch.massage_admin_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- massage_admin_app/lib/web_download_helper.dart
+++
@@ -1,9 +0,0 @@
-// lib/web_download_helper.dart
-import 'dart:html' as html;
-
-void downloadWebFile(String csvData, String fileName) {
-  final bytes = Uri.encodeComponent(csvData);
-  final anchor = html.AnchorElement(href: "data:text/csv;charset=utf-8,$bytes")
-    ..setAttribute("download", fileName)
-    ..click();
-}
